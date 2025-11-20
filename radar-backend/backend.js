df7d7cc352583ce237509fa2ba68b5b767936f2c
AndersonVelozo <andersonlinharesvelozo@hotmail.com>
2025-11-19 21:47:49 -0300

    corsrender

--- radar-backend/backend.js
+++ radar-backend/backend.js
@@ -268,6 +268,7 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "https://andersonvelozo.github.io",
+      "https://testessssss.onrender.com", // 👈 ADICIONADO
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
